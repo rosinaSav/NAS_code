09046d0761e10665285ebe4a7ccdfa55395f810a
rosina <rosinasavisaar@gmail.com>
2018-01-25 17:56:41 +0000

    merged bed_ops.py and bam_ops.py into one file

--- tests_bam_ops.py
+++ tests_bam_ops.py
@@ -5,6 +5,15 @@ import unittest
 
 class Test_bam_ops(unittest.TestCase):
 
+    def test_extract_exons(self):
+        gtf = "test_data/test_extract_exons.gtf"
+        observed = "test_data/test_extract_exons_observed.bed"
+        gen.remove_file(observed)
+        extract_exons(gtf, observed)
+        expected = gen.read_many_fields("test_data/test_extract_exons_expected.bed", "\t")
+        observed = gen.read_many_fields(observed, "\t")
+        self.assertEqual(expected, observed)
+
     def test_group_flags(self):
         input_bed = "test_data/test_tabix.bed"
         observed = "test_data/test_group_flags_observed.bed"
