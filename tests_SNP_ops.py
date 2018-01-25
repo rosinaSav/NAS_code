21819cae5140ced659c1025b2c32752ae9b3a675
Rosina Savisaar <rs949@bssv-watson.campus.bath.ac.uk>
2018-01-25 13:24:58 +0000

    updated paths in tests_SNP_ops.py

--- tests_SNP_ops.py
+++ tests_SNP_ops.py
@@ -10,7 +10,7 @@ class Test_SNP_ops(unittest.TestCase):
         expected = gen.read_many_fields("test_data/test_tabix_expected.txt", "\t")
         observed = "test_data/test_tabix_observed.txt"
         gen.remove_file(observed)
-        vcf = "../../general/1000genomes/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.gz"
+        vcf = "../source_data/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.gz"
         tabix(bed_file, observed, vcf)
         observed = gen.read_many_fields(observed, "\t")
         self.assertEqual(sorted(observed), sorted(expected))
@@ -22,8 +22,8 @@ class Test_SNP_ops(unittest.TestCase):
         observed = "test_data/test_tabix_samples_observed.txt"
         gen.remove_file(observed)
         gen.remove_file(observed + ".gz")
-        vcf_folder = "../../general/1000genomes/per_sample_vcfs"
-        panel_file = "../../general/1000genomes/integrated_call_samples_v3.20130502.ALL.panel"
+        vcf_folder = "../source_data/per_sample_vcfs"
+        panel_file = "../source_data/integrated_call_samples_v3.20130502.ALL.panel"
         tabix_samples(bed_file, observed + ".gz", panel_file, vcf_folder, superpop = "AFR", vcftools_path = "../../../Software/vcftools")
         gen.run_process(["bgzip", "-d", observed + ".gz"])
         with open(observed) as file:
