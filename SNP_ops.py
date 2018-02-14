3e712ea8ab4f74b132c804223b51bb1ba52e02f0
rosina <rosinasavisaar@gmail.com>
2018-02-14 12:35:49 +0000

    fixed error in get_snp_relative_exon_position where antisense exons were handled incorrectly. Added a check to get_snp_relative_cds_position to catch microexons that have the sequence of a stop.

--- SNP_ops.py
+++ SNP_ops.py
@@ -6,89 +6,6 @@ import string
 import collections
 import re
 
-def get_relative_SNPs(CDSs, SNP_file_name, CDS_SNP_file_name, seqs, names, genome, get_new_SNPs = False, parse_SNPs = False, remove_GT = False):
-    if get_new_SNPs:
-        flat_CDS = [[j[0] for j in i] for i in list(CDSs.values())]
-        CDS_bed = "temp_data/temp_{0}.bed".format(random.random())
-        write_features_to_bed(flat_CDS, CDS_bed, modify_chr_ids = True)
-        tabix(CDS_bed, SNP_file_name, genome)
-        os.remove(CDS_bed)
-    if parse_SNPs:
-        relative_coords_dict = {i: {"positions": [], "to_remove": [], "var_bases": [], "MAFs": [], "allele_counts": []} for i in list(CDSs.keys())}
-        counter = 0
-        with open(SNP_file_name) as file:
-            error_counter = 0
-            #loop over bed file with SNPs
-            for line in file:
-                if line[0] != "#":
-                    if counter % 1000 == 0:
-                        print(counter)
-                    counter = counter + 1
-                    bed_record = line.split("\t")
-                    info = bed_record[6].split("$")
-                    #reported ancestral base
-                    ref_base = info[1]
-                    #filter out anything that isn't simple SNPs
-                    var_base = info[2].split(",")
-                    var_base_number = len(var_base)
-                    var_base = [i for i in var_base if i in nc._canon_bases_]
-                    trans = bed_record[3]
-                    #convert bed record to indices relative to the CDS
-                    #mismatch_warning_only = True means that if the SNP coordinates don't map onto the CDS,
-                    #return an error message but don't crash.
-                    current_index = bed_to_CDS_indices(bed_record, CDSs[trans], mismatch_warning_only = True)[0]
-                    #this is so you could remove the stuff you filter out for various reasons also from the monomorphic set
-                    relative_coords_dict[trans]["to_remove"].append(str(current_index))
-                    #the SNP bed files contain information on which transcript they overlap
-                    if current_index != ("error"):
-                        if len(ref_base) == 1:
-                            #filtering out polymorphisms with more than 2 segregating alleles
-                            #note that you need to check the number both before and after filtering out non-canonical bases
-                            if var_base_number == 1 and len(var_base) == 1:
-                            #check if the reported ancestral allele matches the base that is at that position in the CDS
-                            #it's expected that sometimes it won't but it should most of the time if everything's worked out properly
-                                strand = CDSs[trans][0][0][6]
-                                var_base = var_base[0]
-                                if strand == "-":
-                                    ref_base = nc.rev_comp(ref_base)
-                                    var_base = nc.rev_comp(var_base)
-                                CDS_base = seqs[names.index(trans)][current_index]
-                                if ref_base != CDS_base:
-                                    print("PROBLEM!")
-                                    print(ref_base)
-                                    print(CDS_base)
-                                    print(var_base)
-                                    print("\n")
-                                elif remove_GT and ref_base == "G" and var_base == "T":
-                                    pass
-                                else:
-                                    relative_coords_dict[trans]["positions"].append(str(current_index))
-                                    relative_coords_dict[trans]["var_bases"].append(str(var_base))
-                                    MAF_MAC = info[4].split(";")
-                                    MAC = MAF_MAC[0].split("=")[1]
-                                    MAF = MAF_MAC[1].split("=")[1]
-                                    relative_coords_dict[trans]["MAFs"].append(str(MAF))
-                                    relative_coords_dict[trans]["allele_counts"].append(str(MAC))
-                    else:
-                        error_counter = error_counter + 1
-        with open(CDS_SNP_file_name, "w") as file:
-            for trans in relative_coords_dict:
-                to_write = zip(relative_coords_dict[trans]["positions"], relative_coords_dict[trans]["var_bases"], relative_coords_dict[trans]["MAFs"], relative_coords_dict[trans]["allele_counts"])
-                to_write = "\t".join([trans, "|".join([",".join(i) for i in to_write]), ",".join(relative_coords_dict[trans]["to_remove"])])
-                file.write(to_write)
-                file.write("\n")
-        relative_coords_dict = {}
-        print("Number of conversion errors:")
-        print(error_counter)
-
-    current_SNPs = rw.read_many_fields(CDS_SNP_file_name, "\t")
-    current_SNPs_to_remove = list_to_dict(current_SNPs, 0, 2)
-    current_SNPs_to_remove = {i: [int(j) for j in current_SNPs_to_remove[i].split(",") if j != "error"] for i in current_SNPs_to_remove if current_SNPs_to_remove[i]}
-    current_SNPs = list_to_dict(current_SNPs, 0, 1)
-    current_SNPs = {i: [j.split(",") for j in current_SNPs[i].split("|")] for i in current_SNPs if current_SNPs[i]}
-    current_SNPs = {i: {int(j[0]): (j[1], float(j[2])) for j in current_SNPs[i]} for i in current_SNPs}
-    return(current_SNPs, current_SNPs_to_remove)
-
 def tabix(bed_file, output_file, vcf, process_number = None):
     '''
     Given a bed file, use tabix to get overlapping 1000Genomes SNPs.
@@ -320,32 +237,7 @@ def tabix_samples(bed_file, output_file_name, panel_file, vcf_folder, superpop =
         gen.remove_file(concat_file)
     gen.remove_file(sort_file)
 
-def get_snp_relative_exon_position(intersect_file):
-    '''
-    Get the relative position of a snp within the exon it is found. Used as an intermediate step
-    before calculating the snp position in the cds using get_snp_cds_relative_position.
-    '''
-    #read the intersects file
-    cds_strands = cds_exons = collections.defaultdict()
-    relative_positions = []
-    intersects = gen.read_many_fields(intersect_file, "\t")
-    for intersect in intersects:
-        #get strand of exon
-        entry_regex = re.compile("(\w+)\.(\d+)(\..*)*")
-        trans = re.search(entry_regex, intersect[3]).group(1)
-        cds_strands[trans] = intersect[5]
-        #get the features
-        feature_start = intersect[1]
-        feature = intersect[3]
-        snp_start = intersect[7]
-        #get the position of the snp compared with the feature
-        relative_position = int(snp_start) - int(feature_start)
-        #replace . field with relative position
-        intersect[11] = str(relative_position)
-        relative_positions.append(intersect)
-    return(relative_positions)
-
-def get_snp_relative_cds_position(snp_exon_realtive_positions, snp_cds_position_output, fasta_interval_file):
+def get_snp_relative_cds_position(snp_exon_relative_positions, snp_cds_position_output, fasta_interval_file):
     '''
     Get the position of the snp within a CDS using the relative positions of snps in the features they are found
     '''
@@ -354,15 +246,26 @@ def get_snp_relative_cds_position(snp_exon_realtive_positions, snp_cds_position_
     cds_exons = collections.defaultdict(lambda: collections.defaultdict())
     entry_regex = re.compile("(\w+)\.(\d+)(\..*)*")
     stops = ["TAA", "TAG", "TGA"]
+    stop_dummy = 99999
     #iterate through the fasta entries, sorting by exon (needed in case fasta is not sorted)
     for i, name in enumerate(interval_names):
         name_regex = re.search(entry_regex, name)
+        trans_name = name_regex.group(1)
+        #if you had an internal micro-exon that had the sequence of a stop, the below procedure would lead to an error
+        #this is extremely unlikely
+        #however, I'm adding a check just to be sure
         if interval_seqs[i] in stops:
             #set stop codon id to arbitrarily high number
-            exon = 99999
+            exon = stop_dummy
+            if trans_name in cds_exons:
+                if stop_dummy in cds_exons[trans_name]:
+                    print("The CDS from transcript {0} appears to contain two stops!".format(trans_name))
+                    print(cds_exons[trans_name])
+                    raise Exception
         else:
             exon = int(name_regex.group(2))
-        cds_exons[name_regex.group(1)][exon] = interval_seqs[i]
+        #I changed it to store the length and not the sequence itself to go easier on RAM
+        cds_exons[trans_name][exon] = len(interval_seqs[i])
 
     #set up dict to hold the feature positions relative to the cds
     cds_features_relative_positions = collections.defaultdict(lambda: collections.defaultdict())
@@ -371,11 +274,11 @@ def get_snp_relative_cds_position(snp_exon_realtive_positions, snp_cds_position_
         total = 0
         for exon in sorted(cds_exons[cds]):
             cds_features_relative_positions[cds][exon] = total
-            total += len(cds_exons[cds][exon])
+            total += cds_exons[cds][exon]
 
     #now get the relative position of the snp within the cds
     with open(snp_cds_position_output, "w") as output:
-        for snp in snp_exon_realtive_positions:
+        for snp in snp_exon_relative_positions:
             cds_id = snp[3]
             cds_id_meta = re.search(entry_regex, cds_id)
             snp_cds = cds_id_meta.group(1)
@@ -385,6 +288,41 @@ def get_snp_relative_cds_position(snp_exon_realtive_positions, snp_cds_position_
             snp_cds_position = snp_exon_position + cds_features_relative_positions[snp_cds][int(snp_exon_id)]
             snp[11] = str(snp_cds_position)
             output.write("{0}\n".format("\t".join(snp)))
+
+def get_snp_relative_exon_position(intersect_file):
+    '''
+    Get the relative position of a snp within the exon it is found. Used as an intermediate step
+    before calculating the snp position in the cds using get_snp_cds_relative_position.
+    '''
+    #read the intersects file
+    cds_strands = collections.defaultdict()
+    relative_positions = []
+    intersects = gen.read_many_fields(intersect_file, "\t")
+    for intersect in intersects:
+        #get strand of exon
+        entry_regex = re.compile("(\w+)\.(\d+)(\..*)*")
+        trans = re.search(entry_regex, intersect[3]).group(1)
+        strand = intersect[5]
+        cds_strands[trans] = strand
+        #get the features
+        feature_start = intersect[1]
+        feature_end = intersect[2]
+        feature = intersect[3]
+        snp_start = intersect[7]
+        #get the position of the snp compared with the feature
+        if strand == "+":
+            relative_position = int(snp_start) - int(feature_start)
+        elif strand == "-":
+            #-1 because we're in base 0
+            relative_position = (int(feature_end) - 1) - int(snp_start)
+        else:
+            print("Incorrect strand: {0}.".format(strand))
+            print(intersect)
+            raise Exception
+        #replace . field with relative position
+        intersect[11] = str(relative_position)
+        relative_positions.append(intersect)
+    return(relative_positions)
 
 def get_snp_change_status(snp_cds_relative_positions, cds_fasta, ptcs_output_file, others_output_file):
 
