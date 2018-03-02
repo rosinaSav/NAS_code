41eb4ca85e9b7509ea4bdd542a717838c9a7ad82
Liam Abrahams <l.abrahams@bath.ac.uk>
2018-03-02 10:32:17 +0000

    add ese stop codon simulations

---
+++ ese_stops_simulation.py
@@ -0,0 +1,147 @@
+import simulate_eses_ops as se
+import generic as gen
+
+ese_sets = {
+    "ESR": "./ese_data/ESR/ESR.txt",
+    "Ke400_ESEs": "./ese_data/Ke400_ESEs/Ke400_ESEs.txt",
+    "PESE": "./ese_data/PESE/PESE.txt",
+    "RESCUE": "./ese_data/RESCUE/RESCUE.txt",
+    "INT3": "./ese_data/CaceresHurstESEs_INT3/CaceresHurstESEs_INT3.txt",
+    "RBP_motifs": "./ese_data/RBP_motifs/RBP_motifs.txt",
+    "RBP_motifs_filtered": "./ese_data/RBP_motifs/filtered_RBP_motifs_nd.txt"
+}
+
+def run_simulations(simulation_sets, required_simulations):
+
+    for ese_set in simulation_sets:
+        motifs = []
+        motif_list = []
+
+        gen.remove_file(ese_set[1])
+        gen.remove_file(ese_set[2])
+
+        #get motifs
+        if ese_set[0] == "RBP_motifs_filtered":
+            motifs = []
+            motif_list = []
+            #read the motif ids and seqs from fasta
+            raw_motif_ids, raw_motifs = gen.read_fasta(ese_sets[ese_set[0]])
+            index_list = []
+            if len(ese_set) > 3 and ese_set[3] > 0:
+                # restrict to rbp motifs with nd > 0
+                index_list = [i for i, motif_id in enumerate(raw_motif_ids) if float(motif_id.split('|')[1]) > 0]
+            elif len(ese_set) > 3 and ese_set[3] < 0:
+                # restrict to rbp motifs with nd > 0
+                index_list = [i for i, motif_id in enumerate(raw_motif_ids) if float(motif_id.split('|')[1]) < 0]
+            else:
+                index_list = [i for i, motif_id in enumerate(raw_motif_ids)]
+            #extract the motifs
+            [motif_list.extend(raw_motifs[i].split('|') for i in index_list)]
+            [motifs.extend(i) for i in motif_list]
+
+        elif ese_set[0] == "RBP_motifs":
+            raw_motif_ids, raw_motifs = gen.read_fasta(ese_sets[ese_set[0]])
+            motif_list.extend(i.split('|') for i in raw_motifs)
+            [motifs.extend(i) for i in motif_list]
+
+        else:
+
+            motif_list = gen.read_many_fields(ese_sets[ese_set[0]], ",")
+            # get motifs, avoid header
+            motifs = [i[0] for i in motif_list if i[0][0] != "#"]
+
+
+        #if the rbps are split
+        if len(ese_set) > 3:
+
+            print('Starting {0} {1} set...'.format(ese_set[0], ese_set[4]))
+        else:
+            print('Starting {0} set...'.format(ese_set[0]))
+
+        # get the number of stop codons found in the real set
+        real_count = se.get_stop_codon_count(motifs)
+        # generate simulated motifs using motif set
+        print('Simulating...'.format(ese_set[0]))
+        se.generate_motifs_sets(motifs, required_simulations, output_file=ese_set[1])
+        simulated_motif_sets = gen.read_many_fields(ese_set[1], "|")
+        with open(ese_set[2], "w") as output:
+        	output.write('id,stop_count\n')
+        	output.write('real,{0}\n'.format(real_count))
+        	for i, simulated_set in enumerate(simulated_motif_sets):
+        		stop_count = se.get_stop_codon_count(simulated_set)
+        		output.write('{0},{1}\n'.format(i+1, stop_count))
+
+def main():
+
+    description = "Check whether stop codons are depleted in motif sets by simulating the motif set."
+    args = gen.parse_arguments(description, ["required_simulations", "all_sets", "ESR", "Ke", "PESE", "RESCUE", "INT3", "RBP_motifs", "filter_RBPs", "split_RBPs"], flags = [1,2,3,4,5,6,7,8,9])
+    required_simulations, all_sets, ESR, Ke, PESE, RESCUE, INT3, RBP_motifs, filter_RBPs, split_rbps = args.required_simulations, args.all_sets, args.ESR, args.Ke, args.PESE, args.RESCUE, args.INT3, args.RBP_motifs, args.filter_RBPs, args.split_RBPs
+
+
+    if split_rbps and not filter_RBPs:
+        print('You must specify the filtered RBPs if you want to split by ND.')
+        raise Exception
+
+    if not required_simulations:
+        print('You must specify the number of simulations you require.')
+        raise Exception
+
+    #create the output_directory
+    output_directory = "ese_stops_simulations_output"
+    gen.create_directory(output_directory)
+
+    #set up the simulations we want
+    required_sets = []
+    if all_sets:
+        required_sets.extend([i for i in ese_sets])
+    else:
+        if ESR:
+            required_sets.append("ESR")
+        if Ke:
+            required_sets.append("Ke400_ESEs")
+        if PESE:
+            required_sets.append("PESE")
+        if RESCUE:
+            required_sets.append("RESCUE")
+        if INT3:
+            required_sets.append("INT3")
+        if RBP_motifs and not filter_RBPs:
+            required_sets.append("RBP_motifs")
+        if RBP_motifs and filter_RBPs:
+            required_sets.append("RBP_motifs_filtered")
+
+    #check whether any sets have been chosen
+    if len(required_sets) == 0:
+        print("\nPlease choose a motif set to analyse:\n")
+        [print("--{0}".format(i)) for i in sorted(ese_sets)]
+        print("\n")
+        raise Exception
+
+    #create the necessary files
+    simulation_sets = []
+    for ese_set in required_sets:
+        if ese_set == "RBP_motifs_filtered":
+            dir_name = "RBP_motifs"
+        else:
+            dir_name = ese_set
+        #create the output directory for the particular motif set
+        motif_output_directory = "{0}/{1}".format(output_directory, dir_name)
+        gen.create_directory(motif_output_directory)
+        if split_rbps:
+            #if we want to split the rbp motifs based on nd, need to create 2 lots of outputs
+            simulated_set_output_pos_nd = "{0}/{1}_simulants_pos_nd_{2}.txt".format(motif_output_directory, dir_name, required_simulations)
+            output_file_pos_nd = "{0}/{1}_stop_counts_pos_nd_{2}.csv".format(motif_output_directory, dir_name, required_simulations)
+            simulation_sets.append([ese_set, simulated_set_output_pos_nd, output_file_pos_nd, 1, "Positive ND"])
+            simulated_set_output_neg_nd = "{0}/{1}_simulants_neg_nd_{2}.txt".format(motif_output_directory, dir_name, required_simulations)
+            output_file_neg_nd = "{0}/{1}_stop_counts_neg_nd_{2}.csv".format(motif_output_directory, dir_name, required_simulations)
+            simulation_sets.append([ese_set, simulated_set_output_neg_nd, output_file_neg_nd, -1, "Negative ND"])
+        else:
+            #create simulated set output, analysis output file
+            simulated_set_output = "{0}/{1}_simulants_{2}.txt".format(motif_output_directory, dir_name, required_simulations)
+            output_file = "{0}/{1}_stop_counts_{2}.csv".format(motif_output_directory, dir_name, required_simulations)
+            simulation_sets.append([ese_set, simulated_set_output, output_file])
+
+    run_simulations(simulation_sets, int(required_simulations))
+
+if __name__ == "__main__":
+    main()
---
+++ ese_stops_simulation_prelim.R
@@ -0,0 +1,50 @@
+library("ggplot2")
+setwd(getwd())
+
+args = commandArgs(trailingOnly=TRUE)
+
+if(length(args) == 0) {
+  stop("At least one argument must be supplied -number of simulations", call.=FALSE)
+} else {
+   simulations <- args[1]
+}
+
+# set up blank list
+
+if (args[2] == "all_sets") {
+  sets <- c(
+    # "RBP_motifs/RBP_motifs_stop_counts_pos_nd",
+    # "RBP_motifs/RBP_motifs_stop_counts_neg_nd",
+    "ESR/ESR_stop_counts",
+    "INT3/INT3_stop_counts",
+    "Ke400_ESEs/Ke400_ESEs_stop_counts",
+    "PESE/PESE_stop_counts",
+    "RBP_motifs/RBP_motifs_stop_counts",
+    "RESCUE/RESCUE_stop_counts"
+  )
+} else {
+  sets <- c()
+  for (arg in 2:length(args)){
+    sets <- c(sets, args[arg])
+  }
+}
+
+run_plots <- function(simulations) {
+  for (set in sets) {
+    print(set)
+    file_path <- paste("ese_stops_simulations_output/", set, "_" , simulations, ".csv", sep="")
+    file <- read.csv(file_path, sep=",", head=T)
+    simulants <- file[file$id != 'real',]
+    real <- file[file$id == 'real',]
+
+    plot <- ggplot(data=simulants, aes(simulants$stop_count)) +
+      geom_histogram(breaks=seq(min(file$stop_count), max(file$stop_count), by = 2), col="black", fill="white", alpha = .8) +
+      labs(title=paste(sprintf(set), " (", nrow(simulants), " simulations)", sep="")) +
+      labs(x="Stop codon count in all frames", y="Count") +
+      geom_vline(xintercept=file$stop_count[file$id == "real"], lty=2, col="red")
+
+    out_path = paste("ese_stops_simulations_output/", set, "_", simulations, "_hist.pdf", sep="")
+    ggsave(out_path, plot=plot)
+  }
+}
+run_plots(simulations)
