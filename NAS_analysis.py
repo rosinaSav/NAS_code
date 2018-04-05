dca93d86be745844d22e4ec367b7fbe02a0fd236
l.abrahams@bath.ac.uk <l.abrahams@bath.ac.uk>
2018-04-05 16:05:44 +0100

    monomorphic-sim debug

--- NAS_analysis.py
+++ NAS_analysis.py
@@ -51,7 +51,7 @@ def run_ptc_monomorpphic_simulation_instance(simulations, out_prefix, simulation
         counter = gen.update_counter(counter, 10, "SIMULATION ")
 
         #setup a folder to contain the individual simulation inside the simulations output
-        simulation_instance_folder = "{0}/ptc_monomorphic_simulation_run_{1}".format(simulation_output_folder, simulation_number)
+        simulation_instance_folder = "{0}/ptc_monomorphic_simulation_run_{2}".format(simulation_output_folder, simulation_number)
         if not use_old_sims:
             gen.create_strict_directory(simulation_instance_folder)
         else:
@@ -60,7 +60,7 @@ def run_ptc_monomorpphic_simulation_instance(simulations, out_prefix, simulation
         #generate pseudo ptc snps
         pseudo_monomorphic_ptc_file = "{0}/{1}_pseudo_monomorphic_ptc_file_{2}.txt".format(simulation_instance_folder, out_prefix, simulation_number)
         if (not use_old_sims) or (not(os.path.isfile(pseudo_monomorphic_ptc_file))):
-            so.generate_pesudo_monomorphic_ptcs(ptc_file, nt_indices_files, output_file)
+            so.generate_pesudo_monomorphic_ptcs(ptc_file, nt_indices_files, pseudo_monomorphic_ptc_file)
 
         #filter the exon junctions file to only leave those junctions that flank exons retained in the previous step when generating pseudo ptcs
         pseudo_monomorphic_ptc_exon_junctions_file = "{0}/filtered_exon_junctions_{1}.bed".format(simulation_instance_folder, simulation_number)
