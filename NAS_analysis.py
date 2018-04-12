29a11205dcc1558e0a33fad4b89e6459ffada1a1
Liam A <l.abrahams@bath.ac.uk>
2018-04-12 16:03:00 +0100

    debug call snps

--- NAS_analysis.py
+++ NAS_analysis.py
@@ -447,6 +447,8 @@ def main():
         samples_in_vcf = file.readlines()
     samples_in_vcf = [i.rstrip("\n") for i in samples_in_vcf]
     sample_names = [i for i in sample_names if i in samples_in_vcf]
+    # uncommment for debug
+    sample_names = [samples_names[0]]
     sample_file = "{0}_sample_file.txt".format(out_prefix)
 
     # create a fasta containing all sequences for exons with snp
