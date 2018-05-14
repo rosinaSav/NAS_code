b77c7e82854339b4248a52db1d6648b0e0cc2c4f
l.abrahams@bath.ac.uk <l.abrahams@bath.ac.uk>
2018-05-14 08:18:37 +0100

    transfer commit

--- disease_snps.R
+++ disease_snps.R
@@ -1,8 +1,8 @@
-file <- read.table("./results/disease_snps/disease__analysis_ptc_info.txt", head=T, sep=",")
-hist(file$min_dist, breaks=seq(min(file$min_dist),max(file$min_dist)+10, by=10), freq=F)
+file1 <- read.table("./results/disease_snps/disease__analysis_ptc_info.txt", head=T, sep=",")
+hist(file1$min_dist, breaks=seq(min(file1$min_dist),max(file1$min_dist)+10, by=10), freq=F)
 
-file <- read.table("./results/disease_snps/disease__analysis_other_info.txt", head=T, sep=",")
-hist(file$min_dist, breaks=seq(min(file$min_dist),max(file$min_dist)+10, by=10), freq = F)
+file2 <- read.table("./results/disease_snps/disease__analysis_other_info.txt", head=T, sep=",")
+hist(file2$min_dist, breaks=seq(min(file2$min_dist),max(file2$min_dist)+10, by=10), freq = F)
 
-nrow(file[file$min_dist<70,])/nrow(file)
-nrow(file[file$min_dist<70,])
+nrow(file1[file1$min_dist<70,])/nrow(file1)
+nrow(file2[file2$min_dist<70,])/nrow(file2)
