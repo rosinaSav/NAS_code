66ed8d4a9582e4a582ad9c9736f9099dc7ea591d
l.abrahams@bath.ac.uk <l.abrahams@bath.ac.uk>
2018-06-13 17:14:42 +0100

    update to not automatically run if not require files

--- disease_snps.py
+++ disease_snps.py
@@ -40,7 +40,7 @@ def main():
     exon_bed = "{0}_coding_exons.bed".format(results_prefix)
     disease_snp_intersect_file_vcf = "{0}/disease_snp_intersect.vcf".format(output_directory)
     disease_snp_intersect_file_bed = "{0}/disease_snp_intersect.bed".format(output_directory)
-    if intersect_snps or not os.path.isfile(disease_snp_intersect_file_vcf) or not os.path.isfile(disease_snp_intersect_file_bed):
+    if intersect_snps:
         print("Intersecting snps with exons")
         so.intersect_snps_parallel(exon_bed, disease_snps_file, disease_snp_intersect_file_vcf)
         so.intersect_vcf_to_bed(exon_bed, disease_snp_intersect_file_vcf, disease_snp_intersect_file_bed, change_names = True)
@@ -49,7 +49,7 @@ def main():
     full_bed = "{0}_CDS.bed".format(results_prefix)
     disease_snps_relative_exon_positions = "{0}/disease_snp_relative_exon_positions.bed".format(output_directory)
     disease_snps_relative_cds_positions = "{0}/disease_snp_relative_cds_positions.bed".format(output_directory)
-    if get_relative_positions or not os.path.isfile(disease_snps_relative_exon_positions) or not os.path.isfile(disease_snps_relative_cds_positions):
+    if get_relative_positions:
         print("Getting snp relative positions...")
         so.get_snp_relative_exon_position(disease_snp_intersect_file_bed, disease_snps_relative_exon_positions)
         # output to var because this is how the function was made
@@ -60,7 +60,7 @@ def main():
     cds_fasta = "{0}_CDS.fasta".format(results_prefix)
     disease_ptcs_file = "{0}/disease_ptcs.txt".format(output_directory)
     disease_other_file = "{0}/disease_other_snps.txt".format(output_directory)
-    if get_snp_status or not os.path.isfile(disease_ptcs_file) or not os.path.isfile(disease_other_file) or not os.path.isfile(cds_fasta):
+    if get_snp_status:
         print("Getting snp status...")
         so.get_snp_change_status(disease_snps_relative_cds_positions, cds_fasta, disease_ptcs_file, disease_other_file)
 
