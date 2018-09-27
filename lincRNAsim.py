96c9130c2b8c013604027a911dd8a1d46a4bc7cf
l.abrahams@bath.ac.uk <l.abrahams@bath.ac.uk>
2018-09-27 14:20:26 +0100

    add cores

--- lincRNAsim.py
+++ lincRNAsim.py
@@ -57,32 +57,35 @@ def get_seq_dints(seq):
     dints2 = re.findall(".{2}", seq[1:])
     dints.extend(dints1)
     dints.extend(dints2)
-    nts.extend(list(set(seq)))
+    nts.extend(list(seq))
 
     return dints, nts
 
 def get_flank_seqs(seqs_dict, start, stop):
     flanks = []
+    cores = []
     for id in seqs_dict:
         seq = seqs_dict[id]
         # need a seq of at least 8 nts to get at least 1 dint from each flank
-        if len(seq) >= 8 and len(seq) <= 138:
-            mid = len(seq)/2
-            if not isinstance(mid, int):
-                mid = int(mid)
-                flank_five_prime = seq[start-1:mid+1]
-                flank_three_prime = seq[len(seq)-mid:-(start-1)]
-            else:
-                mid = int(mid)
-                flank_five_prime = seq[(start-1):mid]
-                flank_three_prime = seq[len(seq)-mid:-(start-1)]
-            flanks.extend([flank_five_prime, flank_three_prime])
+        # if len(seq) >= 8 and len(seq) <= 138:
+        #     mid = len(seq)/2
+        #     if not isinstance(mid, int):
+        #         mid = int(mid)
+        #         flank_five_prime = seq[start-1:mid+1]
+        #         flank_three_prime = seq[len(seq)-mid:-(start-1)]
+        #     else:
+        #         mid = int(mid)
+        #         flank_five_prime = seq[(start-1):mid]
+        #         flank_three_prime = seq[len(seq)-mid:-(start-1)]
+        #     flanks.extend([flank_five_prime, flank_three_prime])
         if len(seq) > 138:
-                flank_five_prime = seq[start-1:stop]
-                flank_three_prime = seq[len(seq)-stop:-(start-1)]
-                flanks.extend([flank_five_prime, flank_three_prime])
+            flank_five_prime = seq[start-1:stop]
+            flank_three_prime = seq[len(seq)-stop:-(start-1)]
+            flanks.extend([flank_five_prime, flank_three_prime])
+            core = seq[stop:len(seq)-stop]
+            cores.append(core)
 
-    return flanks
+    return flanks, cores
 
 def get_stop_counts(seqs):
     counts = []
@@ -125,42 +128,71 @@ def simulate_seqs(simulations, seqs, dint_list, dint_freqs, nt_list, nt_freqs):
         outputs.append(sim_count)
     return outputs
 
+def run_simulations(simulations, seqs, output_file):
+    real_counts = get_stop_counts(seqs)
+    dints, nts = get_dints(seqs)
+    dint_list = list(set(dints))
+    dint_freqs = [np.divide(dints.count(i), len(dints)) for i in dint_list]
+    nt_list = list(set(nts))
+    nt_freqs = [np.divide(nts.count(i), len(nts)) for i in  nt_list]
+
+    processes = gen.run_in_parallel(simulations, ["foo", seqs, dint_list, dint_freqs, nt_list, nt_freqs], simulate_seqs, workers = os.cpu_count() - 2)
+
+    outputs = []
+    for process in processes:
+        outputs.extend(process.get())
+
+    with open(output_file, "w") as outfile:
+        outfile.write("simulation,count\n")
+        outfile.write("real,{0}\n".format(real_counts))
+        for i,output in enumerate(outputs):
+            outfile.write("{0},{1}\n".format(i+1,output))
+
+def get_excesses(seq_flanks, seq_cores):
+
+    flank_counts = get_stop_counts(seq_flanks)
+    core_counts = get_stop_counts(seq_cores)
+    flank_dints, flank_nts = get_dints(seq_flanks)
+    core_dints, core_nts = get_dints(seq_cores)
+
+
+    # print(len(flank_nts))
+    # print(len(core_nts))
+    print(core_counts, len(core_nts))
+    core_stop_per_nt = np.divide(core_counts, len(core_nts))
+    print(core_stop_per_nt)
+    exp_flank_stops = core_stop_per_nt * len(flank_nts)
+    print(exp_flank_stops, flank_counts)
+    excess = np.divide(flank_counts - exp_flank_stops, sum([flank_counts, core_counts]))
+    print(excess*100)
+
 
 def main():
 
     description = "Check the number of sotp codons in lincRNA set."
-    args = gen.parse_arguments(description, ["bed_file", "genome_fasta", "output_dir", "required_simulations", "get_exons", "run_simulation"], flags = [4,5], ints = [3])
-    bed_file, genome_fasta, output_dir, required_simulations, get_exons, run_simulation = args.bed_file,  args.genome_fasta, args.output_dir, args.required_simulations, args.get_exons, args.run_simulation
+    args = gen.parse_arguments(description, ["bed_file", "genome_fasta", "output_dir", "required_simulations", "get_exons", "run_simulation", "flanks", "cores", "excesses"], flags = [4,5,6,7,8], ints = [3])
+    bed_file, genome_fasta, output_dir, required_simulations, get_exons, run_simulation, flanks, cores, excesses = args.bed_file,  args.genome_fasta, args.output_dir, args.required_simulations, args.get_exons, args.run_simulation, args.flanks, args.cores, args.excesses
 
     gen.create_output_directories(output_dir)
     output_fasta = "{0}/lincRNA_exons.fa".format(output_dir)
 
     get_lincRNA_seqs(bed_file, genome_fasta, output_fasta)
     lincRNA_seqs = get_fasta_seqs(output_fasta)
-    seq_flanks = get_flank_seqs(lincRNA_seqs, 3, 69)
-
-    real_stop_counts = get_stop_counts(seq_flanks)
-
-    dints, nts = get_dints(seq_flanks)
-    dint_list = list(set(dints))
-    dint_freqs = [np.divide(dints.count(i), len(dints)) for i in dint_list]
-    nt_list = list(set(nts))
-    nt_freqs = [np.divide(nts.count(i), len(nts)) for i in nt_list]
+    seq_flanks, seq_cores = get_flank_seqs(lincRNA_seqs, 3, 69)
 
     simulations = list(range(required_simulations))
-    # outputs = simulate_seqs(simulations, seq_flanks, dint_list, dint_freqs, nt_list, nt_freqs)
-    processes = gen.run_in_parallel(simulations, ["foo", seq_flanks, dint_list, dint_freqs, nt_list, nt_freqs], simulate_seqs, workers = os.cpu_count() - 2)
 
-    outputs = []
-    for process in processes:
-        outputs.extend(process.get())
+    if flanks:
+        print("Simluation in flanks...")
+        output_file = "{0}/stop_count_simulation_flank.csv".format(output_dir)
+        run_simulations(simulations, seq_flanks, output_file)
+    if cores:
+        print("Simluation in cores...")
+        output_file = "{0}/stop_count_simulation_core.csv".format(output_dir)
+        run_simulations(simulations, seq_cores, output_file)
 
-    output_file = "{0}/stop_count_simulation.csv".format(output_dir)
-    with open(output_file, "w") as outfile:
-        outfile.write("simulation,count\n")
-        outfile.write("real,{0}\n".format(real_stop_counts))
-        for i,output in enumerate(outputs):
-            outfile.write("{0},{1}\n".format(i+1,output))
+    if excesses:
+        get_excesses(seq_flanks, seq_cores)
 
 
 if __name__ == "__main__":
